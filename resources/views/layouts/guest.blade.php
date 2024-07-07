e897d7fd2d00b346142584ef25da0d069bbf003b
Eliton Campos <eliton.pecas@gmail.com>
2024-07-07 17:15:54 -0300

    View login logo ok

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,3 +1,4 @@
+@php use Illuminate\Support\Facades\URL; @endphp
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
@@ -17,7 +18,7 @@
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
             <div>
-                <img src="{{ asset('./images/logo_organize.png') }}" alt="Logo Organize">
+                <img src="{{URL::asset('../images/logo.png') }}" alt="Logo Organize">
             </div>
 
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
